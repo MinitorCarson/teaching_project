687a5b5f2296b3fa0796f40898c6d8682db0f4ca
Carson <925314768@qq.com>
2020-10-04 11:25:46 +0800

    多添加些英雄
    现在写出来的代码不能成为程序员写的，这种太过于直白，也就是烂，浪费了java这个语言。
    java是用来写可维护代码的，因此不允许这么糟蹋它。

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
+import awkward.hero.Camille;
 import awkward.hero.Diana;
+import awkward.hero.Irelia;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         String name = Main.getPlayerInput();
-        if(name.equals("Diana")){
-            Diana diana = new Diana();
-            diana.r();
+        switch (name){
+            case "Diana":
+                Diana diana = new Diana();
+                diana.r();
+                break;
+            case "Irelia":
+                System.out.println("ddd");
+                Irelia irelia = new Irelia();
+                irelia.r();
+                break;
+            case "Camille":
+                Camille camille = new Camille();
+                camille.r();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + name);
         }
+
     }
 
     private static String getPlayerInput(){
---
+++ src/awkward/hero/Camille.java
@@ -0,0 +1,19 @@
+package awkward.hero;
+
+public class Camille {
+    public void q(){
+        System.out.println("Camille q");
+    }
+
+    public void w(){
+        System.out.println("Camille w");
+    }
+
+    public void e(){
+        System.out.println("Camille e");
+    }
+
+    public void r(){
+        System.out.println("Camille r");
+    }
+}
